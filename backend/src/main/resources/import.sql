1969ac8119c210f208c33759dc2d58a1034f587c
rafaelnunes34 <rafael.nunes34@gmail.com>
2022-08-08 15:52:15 -0300

    Resource

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -18,3 +18,7 @@ INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'ht
 
 INSERT INTO tb_offer(edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITH TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-11-20T03:00:00Z', 1);
 INSERT INTO tb_offer(edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITH TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITH TIME ZONE '2021-12-20T03:00:00Z', 1);
+
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://c.pxhere.com/images/15/da/2135fc8acd22bb3f6ae67c79fe53-1449505.jpg!s2', 1, 1);
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas duvidas', 2, 'https://c.pxhere.com/images/15/da/2135fc8acd22bb3f6ae67c79fe53-1449505.jpg!s2', 2, 1);
+INSERT INTO tb_resource(title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para turmas', 3, 'https://c.pxhere.com/images/15/da/2135fc8acd22bb3f6ae67c79fe53-1449505.jpg!s2', 0, 1);
